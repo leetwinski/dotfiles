e250a758cb7d6a3c0908bf34f133bbd70286c744
Victor Litvintsev <leetwinski@gmail.com>
2023-09-13 13:52:40 +0200

    add json, csv and toml packages

---
+++ custom_conf/prog-data-formats.el
@@ -0,0 +1,13 @@
+(use-package json-mode
+  :ensure t
+  :defer t)
+
+(use-package csv-mode
+  :ensure t
+  :defer t)
+
+(use-package toml-mode
+  :ensure t
+  :defer t)
+
+(provide 'prog-data-formats)
