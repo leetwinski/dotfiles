412dd6517f90aad2121817bff8f84efe6e847996
Victor Litvintsev <leetwinski@gmail.com>
2024-01-03 18:07:48 +0100

    remove obsolete code

--- custom_conf/prog-c.el
+++ custom_conf/prog-c.el
@@ -4,12 +4,4 @@
                     (eldoc-mode 1)
                     (eglot-ensure))))
 
-;; (use-package zig-mode
-;;   :ensure t
-;;   :defer t
-;;   :hook
-;;   (zig-mode . (lambda ()
-;;                 (eldoc-mode 1)
-;;                 (eglot-ensure))))
-
 (provide 'prog-c)
