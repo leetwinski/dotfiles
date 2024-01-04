8f7d138da6a8050b7df08ac9c5ce36d320879771
Victor Litvintsev <leetwinski@gmail.com>
2024-01-04 14:13:35 +0100

    add more babel langs, add polymode, add couple of util functions

--- custom_conf/org-stuff.el
+++ custom_conf/org-stuff.el
@@ -4,6 +4,11 @@
   :config
   (org-roam-db-autosync-mode +1))
 
+(use-package poly-org
+  :ensure t
+  :defer t
+  :hook (org-mode . (lambda () (poly-org-mode t))))
+
 (add-hook 'org-mode-hook (lambda ()
                            (yas-minor-mode t)
                            ;; org-babel settings
@@ -13,6 +18,8 @@
                             '((lisp . t)
                               (clojure . t)
                               (shell . t)
+                              (js . t)
+                              (C . t)
                               (julia . t)))))
 
 (provide 'org-stuff)
