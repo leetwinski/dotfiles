1f6115ac4f30764c982573f770fe0abd42c1bb38
Victor Litvintsev <leetwinski@gmail.com>
2026-05-08 13:47:29 +0200

    update dr config

--- nix/dr/configuration.nix
+++ nix/dr/configuration.nix
@@ -5,16 +5,166 @@
 # NixOS-WSL specific options are documented on the NixOS-WSL repository:
 # https://github.com/nix-community/NixOS-WSL
 
-{ config, lib, pkgs, ... }:
+{ config, pkgs, lib, ... }:
+let
+  unstable = import <unstable> { };
+in {
+  imports =
+    [ # Include the results of the hardware scan.
+      <nixos-avf/avf>
+      <home-manager/nixos>
+    ];
 
-{
-  imports = [
-    # include nixos-avf modules
-    <nixos-avf/avf>
-  ];
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
 
-  # Change default user
-  # avf.defaultUser = "droid";
+  avf.defaultUser = "leet";
+
+  # boot.extraModprobeConfig = ''
+  #     options snd-hda-intel model=auto
+  # '';
+
+  # boot.kernelModules = ["snd_hda_intel"];
+  # boot.kernelParams = [ "snd-intel-dspcfg.dsp_driver=1" ];
+
+  # boot.kernelPackages = unstable.linuxPackages_latest;
+  # boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.kernelPackages = pkgs.linuxPackagesFor pkgs.linux_latest;
+
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Europe/Madrid";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "es_ES.UTF-8";
+    LC_IDENTIFICATION = "es_ES.UTF-8";
+    LC_MEASUREMENT = "es_ES.UTF-8";
+    LC_MONETARY = "es_ES.UTF-8";
+    LC_NAME = "es_ES.UTF-8";
+    LC_NUMERIC = "es_ES.UTF-8";
+    LC_PAPER = "es_ES.UTF-8";
+    LC_TELEPHONE = "es_ES.UTF-8";
+    LC_TIME = "es_ES.UTF-8";
+  };
+
+  # Enable the X11 windowing system.
+  # You can disable this if you're only using the Wayland session.
+  services.xserver.enable = true;
+  services.libinput.enable = true;
+  services.libinput.touchpad.naturalScrolling = true;
+
+  # Enable the KDE Plasma Desktop Environment.
+  # services.displayManager.sddm.enable = true;
+  # services.desktopManager.plasma6.enable = true;
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us,ru";
+    variant = ",";
+    options = "ctrl:nocaps,grp:win_space_toggle";
+  };
+
+  services.xserver.autoRepeatDelay = 200;
+  services.xserver.autoRepeatInterval = 20;
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # hardware.bluetooth.enable = true;
+  # hardware.bluetooth.powerOnBoot = true;
+
+  # systemd.user.services.mpris-proxy = {
+  #   description = "Mpris proxy";
+  #   after = [ "network.target" "sound.target" ];
+  #   wantedBy = [ "default.target" ];
+  #   serviceConfig.ExecStart = "${pkgs.bluez}/bin/mpris-proxy";
+  # };
+
+  # Enable sound with pipewire.
+  security.rtkit.enable = true;
+
+  services.pulseaudio.enable = false;
+  # hardware.pulseaudio.enable = true;
+  # hardware.pulseaudio.support32Bit = true;
+  # hardware.pulseaudio.package = pkgs.pulseaudioFull;
+
+  # services.pipewire = {
+  #   enable = true;
+  #   package = unstable.pipewire;
+  #   alsa.enable = true;
+  #   alsa.support32Bit = true;
+  #   pulse.enable = true;
+  #   # If you want to use JACK applications, uncomment this
+  #   jack.enable = true;
+
+  #   # use the example session manager (no others are packaged yet so this is enabled by default,
+  #   # no need to redefine it in your config for now)
+  #   #media-session.enable = true;
+  # };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.leet = {
+    isNormalUser = true;
+    description = "Victor Litvintsev";
+    extraGroups = [ "networkmanager" "wheel" "docker" ];
+    packages = with pkgs; [
+      # kdePackages.kate
+
+      #  thunderbird
+    ];
+  };
+
+  nix.settings.experimental-features = ["nix-command" "flakes" "ca-derivations"];
+
+  nix.extraOptions = ''
+    extra-experimental-features = nix-command flakes
+  '';
+
+
+  # Install firefox.
+  # programs.firefox.enable = true;
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  # environment.systemPackages = with pkgs; [
+  # #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+  # #  wget
+  #   pkgs.sof-firmware
+  # ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
@@ -23,4 +173,39 @@
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "26.05"; # Did you read the comment?
+
+  security.polkit.enable = true;
+  home-manager.users.leet = (import /home/leet/dotfiles/nix/home.nix);
+
+  virtualisation.docker.enable = true;
+
+  # virtualisation.docker.rootless = {
+  #   enable = true;
+  #   setSocketVariable = true;
+  # };
+
+  programs.dconf.enable = true;
+
+  programs.nix-ld.enable = true;
+  programs.nix-ld.libraries = with pkgs; [
+    # Add any missing dynamic libraries for unpackaged programs
+    # here, NOT in environment.systemPackages
+    openssl
+    libffi
+    blas
+    lapack
+    unstable.tdlib
+    zlib
+    glibc
+    glib
+    libvterm
+    libev
+    libuv
+    ffmpeg-full
+  ];
+
+  programs.gnupg = {
+    agent.enable = true;
+    agent.enableSSHSupport = true;
+  };
 }
